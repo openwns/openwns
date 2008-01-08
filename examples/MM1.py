5a8752719d9defdd03aac26569dca8b6fd7d0218
Marc Schinnenburg <marc@schinnenburg.com>
2008-01-08 15:07:30 +0100

    added some documentation to the MM1 example

--- examples/MM1.py
+++ examples/MM1.py
@@ -7,6 +7,14 @@ import openwns.queuingsystem
 # The variable must be called WNS!!!!
 WNS = openwns.OpenWNS.OpenWNS()
 
-WNS.simulationModel = openwns.queuingsystem.SimpleMM1(.1, .099)
+# mean service time (negative exponentially distributed) for one job in [s]
+meanServiceTime = 0.099
 
+# mean inter arrival time (negative exponentially distributed) of the jobs in [s]
+meanInterArrivalTime = 0.100
+
+# create the M/M/1 simulation model
+WNS.simulationModel = openwns.queuingsystem.SimpleMM1(meanInterArrivalTime, meanServiceTime)
+
+# set simtime to 1000 seconds
 WNS.maxSimTime = 1000
