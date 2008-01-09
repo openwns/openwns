0f3449dafde45818e14027d13b83bb4c3ca6fb82
Marc Schinnenburg <marc@schinnenburg.com>
2008-01-09 17:00:51 +0100

    Adapted M/M/1 example to changed configuration structure

--- examples/MM1.py
+++ examples/MM1.py
@@ -1,20 +1,37 @@
-# import the openWNS module. Contains all sub-classes needed for
-# configuration of openWNS
-import openwns.OpenWNS
+# import the necessary modules
+
+# openwns contains the Simulator class, which is needed for every
+# simulation
+import openwns
+
+# openws.queuingsystem contains the simulation model called
+# "SimpleMM1" which is used in this example
 import openwns.queuingsystem
 
-# create an instance of the openWNS configuration
-# The variable must be called WNS!!!!
-WNS = openwns.OpenWNS.OpenWNS()
 
-# mean service time (negative exponentially distributed) for one job in [s]
-meanServiceTime = 0.099
+### Simulation setup
+#
+# Q: queue with unlimited size
+#
+# W: worker, the job processing time is negative-exponentially
+#    distributed
+#
+# The jobs arrive at the system with an inter arrival time that is
+# negative-exponentially distributed.
+#
+#             ----
+# new jobs --> Q |-->(W)-->
+#             ----
+#
 
-# mean inter arrival time (negative exponentially distributed) of the jobs in [s]
-meanInterArrivalTime = 0.100
 
-# create the M/M/1 simulation model
-WNS.simulationModel = openwns.queuingsystem.SimpleMM1(meanInterArrivalTime, meanServiceTime)
+# create the M/M/1 simulation model configuration (time in seconds)
+mm1 = openwns.queuingsystem.SimpleMM1(meanJobInterArrivalTime = 0.100,
+                                      meanJobProcessingTime   = 0.099)
 
-# set simtime to 1000 seconds
-WNS.maxSimTime = 1000
+# create simulator configuration
+sim = openwns.Simulator(simulationModel = mm1,
+                        maxSimTime      = 1000)
+
+# set the configuration for this simulation
+openwns.setSimulator(sim)
