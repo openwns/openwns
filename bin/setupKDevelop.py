8049e9eedabab648491827f54590c0451b6d646a
Maciej Muehleisen <mue@mue-laptop>
2010-01-09 19:36:57 +0100

    Changed python2.4 to python in setupKDevelop

--- bin/setupKDevelop.py
+++ bin/setupKDevelop.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2.4
+#! /usr/bin/env python
 ###############################################################################
 # This file is part of openWNS (open Wireless Network Simulator)
 # _____________________________________________________________________________
